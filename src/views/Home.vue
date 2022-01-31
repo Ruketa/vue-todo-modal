4f2aaf6a00a4ef94a15b1d7756d2bcd68ea15c6e
Ruketa <tkrkrhr@gmail.com>
2022-02-01 00:19:40 +0900

    add modal component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <Modal/>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from "vue";
+import Modal from "@/views/Modal.vue"
 
-@Options({
+export default defineComponent({
+  name: "Home",
   components: {
-    HelloWorld,
+    Modal
   },
-})
-export default class Home extends Vue {}
+  setup(){
+    return {}
+  }
+}) 
 </script>
---
+++ src/views/Modal.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="overlay">
+    <div class="modal">
+      <div class="modal__header">
+        <h2 class="modal__header__title">
+          次のタスクを選ぼう
+        </h2>
+        <img src="@/assets/close.png" class="modal__header__close" @click="handleClickClose"/>
+    </div>
+    <div class="modal__main">
+      <div v-for="slopeOption in slopeOptions" :key="slopeOption">
+        <div :class="radio_style(slopeOption.value)">
+          <input type="radio" name="slope" :value="slopeOption.value" v-model="selected"/>
+          <label>{{slopeOption.label}}</label>
+        </div>
+      </div>
+    </div>
+    <div class="modal__footer">
+       <button class="modal__footer__button" @click="handleClickCancel">キャンセル</button>
+       <button class="modal__footer__button" theme="primary" @click="handleClickSubmit">決定</button>
+    </div>
+   </div> 
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from "vue";
+
+export default defineComponent({
+  name: "Modal",
+  setup(props, context){
+
+    const radio_style = (slopeValue: string) => {
+      let style = "modal__main__radio"
+      if(slopeValue === selected.value){
+        style = style + " radio__checked"
+      }
+      return style
+    } 
+    const selected= ref("");
+    const slopeOptions = reactive([
+      {
+        value: 0,
+        label: "要件定義",
+      },
+      {
+        value: 1,
+        label: "論理設計",
+      },
+      {
+        value: 2,
+        label: "物理設計",
+      },
+      {
+        value: 3,
+        label: "プログラム",
+      },
+      {
+        value: 4,
+        label: "テスト",
+      },
+    ])
+
+    // ×ボタン押した時のイベントハンドラ
+    const handleClickClose = () => {
+      context.emit("updateModalViewVisible", false)  
+    }
+
+    // キャンセルボタン押した時のイベントハンドラ
+    const handleClickCancel = () => {
+      handleClickClose()
+    }
+
+    // OKボタン押した時のイベントハンドラ
+    const handleClickSubmit = () => {
+      const selectedSlope = slopeOptions.find((opt) => opt.value === Number(selected.value))
+      context.emit("updateSelectedSlope", selectedSlope)
+    }
+
+    return {
+      selected,
+      slopeOptions,
+      
+      radio_style,
+      handleClickClose,
+      handleClickCancel,
+      handleClickSubmit,
+    }
+  }
+}) 
+</script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vw;
+  background: rgba(#000, 0.3)
+}
+
+.modal{
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  width: 100%;
+  max-height: 640px;
+  height: 100%;
+  border: solid #aaa;
+  border-radius: 8px;
+  background-color: #fff;
+  overflow: hidden;
+}
+
+.modal__header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: solid 1px #eee;
+  background-color: #ebf5fe;
+  padding: 16px 16px 12px;
+}
+
+.modal__header__close{
+  width: 16px;
+  height: 16px;
+}
+
+.modal__main{
+  height: 100%;
+  padding: 16px;
+}
+
+.modal__main__radio{
+  display: flex;
+  justify-content: flex-start;
+  border: solid 1px #aaa;
+  border-radius: 4px;
+  padding: 16px;
+  margin-top: 12px;
+}
+
+.radio__checked{
+  color:#eee;
+  font-weight: bold;
+  background-color: #42b983;
+}
+
+
+.modal__footer{
+  display: flex;
+  justify-content: flex-end;
+  padding: 12px 16px 12px;
+  border-top: solid 1px #ddd;
+}
+
+.modal__footer__button{
+  margin-left: 8px
+}
+
+</style>
